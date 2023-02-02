d62b7e527b5187f01ab8d37bdb9f1f1f504e2bb4
gxuud <igaoxian@foxmail.com>
2023-02-02 14:50:11 +0800

    feat: 使用dva

--- src/components/Demo2/index.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Demo2 = () => {
-    return <div>Demo2</div>;
-}
-
-export default Demo2;
--- src/index.ts
+++
@@ -1,4 +0,0 @@
-import Demo1 from './components/Demo1';
-import Demo2 from './components/Demo2';
-
-export default { Demo1, Demo2 };
